09521f6a4b74b7fcfff574ed1a940822de257104
huygia12 <huynguyengia12@gmail.com>
2024-10-24 20:57:58 +0700

    fix: cors

--- src/config/socket-config.ts
+++ src/config/socket-config.ts
@@ -1,8 +1,11 @@
 import { ClientEvents, ServerEvents } from "@/types/socket";
 import { Socket, io } from "socket.io-client";
 
-const socketInstance: Socket<ServerEvents, ClientEvents> = io(
-  import.meta.env.VITE_SERVER_URL
-);
+const localHost = "http://127.0.0.1";
+const serverUrl =
+  import.meta.env.VITE_API_SERVER_URL ||
+  `${localHost}:${import.meta.env.VITE_API_SERVER_PORT}`;
+
+const socketInstance: Socket<ServerEvents, ClientEvents> = io(serverUrl);
 
 export { socketInstance };
