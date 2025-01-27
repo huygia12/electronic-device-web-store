3b2a515956d57efc56843d80241ced4261b8ed65
huygia12 <huynguyengia12@gmail.com>
2025-01-27 19:13:12 +0700

    chore: update temp data file

--- src/pages/data.ts
+++ src/pages/data.ts
@@ -3,43 +3,36 @@ import { Slide } from "@/types/model";
 export const editingSlides: Slide[] = [
   {
     url: "/slideShow1.png",
-    slideID: "slide1",
     ref: "/products",
     index: 1,
   },
   {
     url: "/slideShow2.jpg",
-    slideID: "slide2",
     ref: "/products",
     index: 2,
   },
   {
     url: "/slideShow3.png",
-    slideID: "slide3",
     ref: "/products",
     index: 3,
   },
   {
     url: "/slideShow4.jpg",
-    slideID: "slide4",
     ref: "/products",
     index: 4,
   },
   {
     url: "/slideShow5.jpg",
-    slideID: "slide5",
     ref: "/products",
     index: 5,
   },
   {
     url: "/slideShow6.jpg",
-    slideID: "slide6",
     ref: "/products",
     index: 6,
   },
   {
     url: "/slideShow7.jpg",
-    slideID: "slide7",
     ref: "/products",
     index: 7,
   },
